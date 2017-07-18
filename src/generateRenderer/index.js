3a59d774c39348944ae959dd87a441f10d65fa5d
Thomas Hervey <thomasahervey@gmail.com>
2017-07-18 09:40:20 -0400

    WIP

--- src/generateRenderer/index.js
+++ src/generateRenderer/index.js
@@ -14,20 +14,22 @@ module.exports = generateRenderer
 function generateRenderer (data, params = {}) {
   const options = {}
   options.params = params
+
   if (data.statistics) {
     const stats = data.statistics
     options.classBreaks = stats.map(attributes => {
       if (attributes.classBreaks) { return attributes.classBreaks } // TODO: find a better way to grab classBreaks from stats
     })[0].sort((a, b) => a - b) // sort class breaks
-  } else if (params.classificationDef) {
-    const queriedData = Winnow.query(data, params)
+  } else if (options.params.classificationDef) {
+    const queriedData = Winnow.query(data, options.params)
     const features = queriedData.features
 
     if (features === undefined || features.length === 0) return // if there are no features, return
 
-    const classification = params.classificationDef
+    const classification = options.params.classificationDef
     if (classification.type === 'classBreaksDef') {
       options.classBreaks = createBreaks(features, classification)
+      // TODO: HANDLE GEOMETRY TYPE
     } else if (classification.type === 'uniqueValuesDef') {
       // TODO: handle unique values & potentially call a different renderer
     }
