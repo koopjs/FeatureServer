f10f2efd1426bd1aa58c858366a96b188f7fd9a7
Thomas Hervey <thomasahervey@gmail.com>
2017-08-02 16:57:06 -0400

    Updated README.md

--- test/generateRenderer.js
+++ test/generateRenderer.js
@@ -85,6 +85,7 @@ describe('when a class breaks classification passed in', () => {
   })
   describe('has correct parameters', () => {
     it('should properly return a renderer', () => {
+      console.log(options)
       const response = generateRenderer(data, options)
       response.type.should.equal('classBreaks')
       response.minValue.should.equal(0)
