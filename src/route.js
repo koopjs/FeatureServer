a2729fe239cd225c2b093c33fd57db97ded74e5d
Bob Gray <bob@iceasb.org>
2018-04-04 17:01:11 -0500

    fixing spaces

--- src/route.js
+++ src/route.js
@@ -23,8 +23,8 @@ function route (req, res, geojson, options) {
   })
 
   if (req.query.callback) {
-      req.query.callback = sanitizeCallback(req.query.callback)
-      res.append("Content-Type", "application/javascript")
+    req.query.callback = sanitizeCallback(req.query.callback)
+    res.append("Content-Type", "application/javascript")
   }
 
   if (isNaN(req.query.limit)) req.query.limit = metadata.maxRecordCount || 2000
