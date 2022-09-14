266dd51002aed368f74441ace1bc6d2b7fd84d10
Rich Gwozdz <rgwozdz@esri.com>
2022-09-14 11:35:12 -0700

    Interim commit

---
+++ test/unit/generate-renderer/create-symbol.spec.js
@@ -0,0 +1,11 @@
+/* global describe, it */
+const _ = require('lodash')
+const should = require('should'); // eslint-disable-line
+const {
+  createSymbol
+} = require('../../../lib/generate-renderer/create-symbol')
+
+
+// describe('when creating a symbol', () => {
+  
+// })
--- test/unit/generate-renderer/index.spec.js
+++ test/unit/generate-renderer/index.spec.js
@@ -1,101 +1,469 @@
-const should = require('should') // eslint-disable-line
-const proxyquire = require('proxyquire')
-const sinon = require('sinon')
+const should = require('should'); // eslint-disable-line
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
 
-describe('generate-render', () => {
-  it('should render precalculated statistics', () => {
-    const createClassificationInfosSpy = sinon.spy(function () {
-      return 'class-break-infos'
-    })
-    const generateRenderer = proxyquire('../../../lib/generate-renderer', {
-      './createClassificationInfos': {
-        createClassBreakInfos: createClassificationInfosSpy
+describe('generate-renderer', () => {
+  it('should handle empty data', () => {
+    const generateRenderer = require('../../../lib/generate-renderer');
+
+    const result = generateRenderer();
+
+    result.should.deepEqual({});
+  });
+
+  describe('with pre-calculated statistics', () => {
+    it('should render precalculated statistics', () => {
+      const getGeometrySpy = sinon.spy(function () {
+        return 'esriGeometryPoint';
+      });
+
+      const createSymbolSpy = sinon.spy(function () {
+        return 'symbol';
+      });
+
+      const createColorRampSpy = sinon.spy(function () {
+        return ['color-1', 'color-2'];
+      });
+
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+        './create-symbol': {
+          createSymbol: createSymbolSpy,
+        },
+        './color-ramp': {
+          createColorRamp: createColorRampSpy,
+        },
+      });
+
+      const data = {
+        statistics: {
+          classBreaks: [
+            [0, 10],
+            [21, 30],
+            [11, 20],
+          ],
+        },
+      };
+      const result = generateRenderer(data);
+
+      result.should.deepEqual({
+        type: 'classBreaks',
+        field: '',
+        classificationMethod: '',
+        minValue: 0,
+        classBreakInfos: [
+          {
+            classMinValue: 0,
+            classMaxValue: 10,
+            label: '0-10',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 11,
+            classMaxValue: 20,
+            label: '11-20',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 21,
+            classMaxValue: 30,
+            label: '21-30',
+            description: '',
+            symbol: undefined,
+          },
+        ],
+      });
+
+      getGeometrySpy.calledOnce.should.equal(true);
+      getGeometrySpy.firstCall.args.should.deepEqual([data]);
+      createColorRampSpy.calledOnce.should.equal(true);
+      createColorRampSpy.firstCall.args.should.deepEqual([
+        { breaks: data.statistics.classBreaks },
+      ]);
+      createSymbolSpy.calledTwice.should.equal(true);
+      createSymbolSpy.firstCall.args.should.deepEqual([
+        undefined,
+        'color-1',
+        'esriGeometryPoint',
+      ]);
+      createSymbolSpy.secondCall.args.should.deepEqual([
+        undefined,
+        'color-2',
+        'esriGeometryPoint',
+      ]);
+    });
+
+    it('should render precalculated statistics with default geometry type', () => {
+      const getGeometrySpy = sinon.spy(function () {
+        return;
+      });
+
+      const createSymbolSpy = sinon.spy(function () {
+        return 'symbol';
+      });
+
+      const createColorRampSpy = sinon.spy(function () {
+        return ['color-1', 'color-2'];
+      });
+
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+        './create-symbol': {
+          createSymbol: createSymbolSpy,
+        },
+        './color-ramp': {
+          createColorRamp: createColorRampSpy,
+        },
+      });
+
+      const data = {
+        statistics: {
+          classBreaks: [
+            [0, 10],
+            [21, 30],
+            [11, 20],
+          ],
+        },
+      };
+      const result = generateRenderer(data);
+
+      result.should.deepEqual({
+        type: 'classBreaks',
+        field: '',
+        classificationMethod: '',
+        minValue: 0,
+        classBreakInfos: [
+          {
+            classMinValue: 0,
+            classMaxValue: 10,
+            label: '0-10',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 11,
+            classMaxValue: 20,
+            label: '11-20',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 21,
+            classMaxValue: 30,
+            label: '21-30',
+            description: '',
+            symbol: undefined,
+          },
+        ],
+      });
+
+      getGeometrySpy.calledOnce.should.equal(true);
+      getGeometrySpy.firstCall.args.should.deepEqual([data]);
+      createColorRampSpy.calledOnce.should.equal(true);
+      createColorRampSpy.firstCall.args.should.deepEqual([
+        { breaks: data.statistics.classBreaks },
+      ]);
+      createSymbolSpy.calledTwice.should.equal(true);
+      createSymbolSpy.firstCall.args.should.deepEqual([
+        undefined,
+        'color-1',
+        'esriGeometryPoint',
+      ]);
+      createSymbolSpy.secondCall.args.should.deepEqual([
+        undefined,
+        'color-2',
+        'esriGeometryPoint',
+      ]);
+    });
+  });
+
+  describe('with winnow statistics', () => {
+    it('should throw error when no classificationDef provided', () => {
+      const winnowSpy = sinon.spy(function () {
+        return [
+          [0, 10],
+          [11, 20],
+          [21, 30],
+        ];
+      });
+
+      const getGeometrySpy = sinon.spy(function () {
+        return 'esriGeometryPoint';
+      });
+
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        winnow: {
+          query: winnowSpy,
+        },
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+      });
+
+      try {
+        generateRenderer({ features: ['feature'] }, {});
+        should.fail('should have thrown error');
+      } catch (error) {
+        error.message.should.equal('invalid classification type: ');
       }
-    })
+    });
 
-    const result = generateRenderer({
-      statistics: {
-        classBreaks: [
-          [0, 10], [21, 30], [11, 20]
-        ]
+    it('should throw error when unsupported geometry type', () => {
+      const winnowSpy = sinon.spy(function () {
+        return [
+          [0, 10],
+          [11, 20],
+          [21, 30],
+        ];
+      });
+
+      const getGeometrySpy = sinon.spy(function () {
+        return undefined;
+      });
+
+      const createClassificationInfosSpy = sinon.spy(function () {
+        return 'class-break-infos';
+      });
+
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        winnow: {
+          query: winnowSpy,
+        },
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+        './createClassificationInfos': {
+          createClassBreakInfos: createClassificationInfosSpy,
+        },
+        './validate-classification-definition': () => {
+          return true;
+        },
+      });
+
+      try {
+        generateRenderer(
+          { features: ['feature'] },
+          {
+            classificationDef: {
+              type: 'classBreaksDef',
+              classificationField: 'classification-field',
+              classificationMethod: 'classification-method',
+            },
+          }
+        );
+        should.fail('should have thrown error');
+      } catch (error) {
+        error.message.should.equal(
+          'Dataset geometry type is not supported for renderers.'
+        );
+        error.code.should.equal(400);
       }
-    })
+    });
 
-    result.should.deepEqual({
-      type: 'classBreaks',
-      field: '',
-      classificationMethod: '',
-      minValue: 0,
-      classBreakInfos: 'class-break-infos'
-    })
-    createClassificationInfosSpy.calledOnce.should.equal(true)
-    createClassificationInfosSpy.firstCall.args.should.deepEqual([
-      [[0, 10], [11, 20], [21, 30]],
-      {},
-      undefined
-    ])
-  })
+    it('should calculate breaks and use classBreaksDef for renderer', () => {
+      const winnowSpy = sinon.spy(function () {
+        return [
+          [0, 10],
+          [11, 20],
+          [21, 30],
+        ];
+      });
 
-  it('should calculate breaks and use classBreaksDef for renderer', () => {
-    const winnowSpy = sinon.spy(function () {
-      return [[0, 10], [11, 20], [21, 30]]
-    })
+      const getGeometrySpy = sinon.spy(function () {
+        return 'esriGeometryPoint';
+      });
 
-    const getGeometrySpy = sinon.spy(function () {
-      return 'esriGeometryPoint'
-    })
+      const createSymbolSpy = sinon.spy(function () {
+        return 'symbol';
+      });
 
-    const createClassificationInfosSpy = sinon.spy(function () {
-      return 'class-break-infos'
-    })
+      const createColorRampSpy = sinon.spy(function () {
+        return ['color-1', 'color-2'];
+      });
 
-    const generateRenderer = proxyquire('../../../lib/generate-renderer', {
-      winnow: {
-        query: winnowSpy
-      },
-      '../helpers': {
-        getGeometryTypeFromGeojson: getGeometrySpy
-      },
-      './createClassificationInfos': {
-        createClassBreakInfos: createClassificationInfosSpy
-      }
-    })
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        winnow: {
+          query: winnowSpy,
+        },
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+        './color-ramp': {
+          createColorRamp: createColorRampSpy,
+        },
+        './create-symbol': { createSymbol: createSymbolSpy },
+      });
 
-    const result = generateRenderer({ features: ['feature'] }, {
-      classificationDef: {
-        type: 'classBreaksDef',
-        classificationField: 'classification-field',
-        classificationMethod: 'classification-method'
-      }
-    })
+      const result = generateRenderer(
+        { features: ['feature'] },
+        {
+          classificationDef: {
+            type: 'classBreaksDef',
+            classificationField: 'classification-field',
+            classificationMethod: 'classification-method',
+          },
+        }
+      );
 
-    result.should.deepEqual({
-      type: 'classBreaks',
-      field: 'classification-field',
-      classificationMethod: 'classification-method',
-      minValue: 0,
-      classBreakInfos: 'class-break-infos'
-    })
+      result.should.deepEqual({
+        type: 'classBreaks',
+        field: 'classification-field',
+        classificationMethod: 'classification-method',
+        minValue: 0,
+        classBreakInfos: [
+          {
+            classMinValue: 0,
+            classMaxValue: 10,
+            label: '0-10',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 11,
+            classMaxValue: 20,
+            label: '11-20',
+            description: '',
+            symbol: 'symbol',
+          },
+          {
+            classMinValue: 21,
+            classMaxValue: 30,
+            label: '21-30',
+            description: '',
+            symbol: undefined,
+          },
+        ],
+      });
 
-    createClassificationInfosSpy.calledOnce.should.equal(true)
-    createClassificationInfosSpy.firstCall.args.should.deepEqual([
-      [[0, 10], [11, 20], [21, 30]],
-      {
-        type: 'classBreaksDef',
-        classificationField: 'classification-field',
-        classificationMethod: 'classification-method'
-      },
-      'esriGeometryPoint'
-    ])
-    getGeometrySpy.calledOnce.should.equal(true)
-    getGeometrySpy.firstCall.args.should.deepEqual([{ features: ['feature'] }])
-    winnowSpy.calledOnce.should.equal(true)
-    winnowSpy.firstCall.args.should.deepEqual([{ features: ['feature'] }, {
-      classificationDef: {
-        type: 'classBreaksDef',
-        classificationField: 'classification-field',
-        classificationMethod: 'classification-method'
-      }
-    }])
-  })
-})
+      getGeometrySpy.calledOnce.should.equal(true);
+      getGeometrySpy.firstCall.args.should.deepEqual([
+        { features: ['feature'] },
+      ]);
+      winnowSpy.calledOnce.should.equal(true);
+      winnowSpy.firstCall.args.should.deepEqual([
+        { features: ['feature'] },
+        {
+          classificationDef: {
+            type: 'classBreaksDef',
+            classificationField: 'classification-field',
+            classificationMethod: 'classification-method',
+          },
+          geometryType: 'esriGeometryPoint',
+        },
+      ]);
+    });
+
+    it('should calculate breaks and use uniqueValueDef for renderer', () => {
+      const winnowSpy = sinon.spy(function () {
+        return [
+          {
+            count: 80,
+            Genus: 'MAGNOLIA',
+          },
+          {
+            count: 77,
+            Genus: 'QUERCUS',
+          },
+          {
+            count: 24,
+            Genus: 'JACARANDA',
+          },
+        ];
+      });
+
+      const getGeometrySpy = sinon.spy(function () {
+        return 'esriGeometryPoint';
+      });
+
+      const createSymbolSpy = sinon.spy(function () {
+        return 'symbol';
+      });
+
+      const createColorRampSpy = sinon.spy(function () {
+        return ['color-1', 'color-2'];
+      });
+
+      const generateRenderer = proxyquire('../../../lib/generate-renderer', {
+        winnow: {
+          query: winnowSpy,
+        },
+        '../helpers': {
+          getGeometryTypeFromGeojson: getGeometrySpy,
+        },
+        './color-ramp': {
+          createColorRamp: createColorRampSpy,
+        },
+        './create-symbol': { createSymbol: createSymbolSpy },
+      });
+
+
+      const result = generateRenderer(
+        { features: ['feature'] },
+        {
+          classificationDef: {
+            type: 'uniqueValueDef',
+            uniqueValueFields: ['Genus'],
+            fieldDelimiter: ',',
+          },
+        }
+      );
+
+      result.should.deepEqual({
+        type: "uniqueValue",
+        field1: "Genus",
+        field2: "",
+        field3: "",
+        fieldDelimiter: ",",
+        defaultSymbol: {
+        },
+        defaultLabel: "",
+        uniqueValueInfos: [
+          {
+            value: "MAGNOLIA",
+            count: 80,
+            label: "MAGNOLIA",
+            description: "",
+            symbol: "symbol",
+          },
+          {
+            value: "QUERCUS",
+            count: 77,
+            label: "QUERCUS",
+            description: "",
+            symbol: "symbol",
+          },
+          {
+            value: "JACARANDA",
+            count: 24,
+            label: "JACARANDA",
+            description: "",
+            symbol: undefined,
+          },
+        ],
+      });
+
+      getGeometrySpy.calledOnce.should.equal(true);
+      getGeometrySpy.firstCall.args.should.deepEqual([
+        { features: ['feature'] },
+      ]);
+      winnowSpy.calledOnce.should.equal(true);
+      winnowSpy.firstCall.args.should.deepEqual([
+        { features: ['feature'] },
+        {
+          classificationDef: {
+            type: 'uniqueValueDef',
+            fieldDelimiter: ',',
+            uniqueValueFields: ['Genus'],
+          },
+          geometryType: 'esriGeometryPoint',
+        },
+      ]);
+    });
+  });
+});
